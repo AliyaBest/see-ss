b6c4dc0f7a623a22f6918ccbfeccc97169c30bbb
AliyaBest <aliyabest@gmail.com>
2020-11-23 13:09:18 -0400

    UPDATE: added background color code to code preview section

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -3,95 +3,102 @@ import { connect } from "react-redux";
 import { AVAILABLE_COLORS, pickColor } from "../redux/colors";
 import "./App.css";
 
-
 export class Colors extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
-    this.state ={
-      selectedOption: "text"
-    }
+    this.state = {
+      selectedOption: "text",
+      color: "gainsboro",
+      background: "white"
+    };
 
-    this.handleOptionChange = this.handleOptionChange.bind(this)
-    this.chooseColor = this.chooseColor.bind(this)
+    this.handleOptionChange = this.handleOptionChange.bind(this);
+    this.chooseColor = this.chooseColor.bind(this);
   }
 
-  componentDidMount(){
-    this.props.pickColor()
+  componentDidMount() {
+    this.props.pickColor();
   }
 
-  handleOptionChange(evt){
+  handleOptionChange(evt) {
     this.setState({
       selectedOption: evt.target.value
-    })
+    });
   }
 
   chooseColor(evt) {
-    this.props.pickColor(evt)
-    const codePreview = document.getElementById('codepreview')
+    this.props.pickColor(evt);
 
-    if(this.state.selectedOption==='text'){
-      document.getElementById('preview-text').style.color = evt.color;
+    if (this.state.selectedOption === "text") {
+      document.getElementById("preview-text").style.color = evt.color;
+      this.setState({
+        color: evt.color
+      });
+    } else if (this.state.selectedOption === "background") {
+      document.getElementById("preview-text").style.backgroundColor = evt.color;
+      this.setState({
+        background: evt.color
+      });
     }
-
-    else if (this.state.selectedOption==='background'){
-      document.getElementById('preview-text').style.backgroundColor = evt.color;
-    }
-    codePreview.innerHTML = `{ color: ${evt.color}; }`
   }
-
-
 
   render() {
     const colors = AVAILABLE_COLORS;
-    let codeSnippet= "{ color: "
 
     return (
       <div>
         <h6>COLOR OPTIONS</h6>
 
-        <form id = "textOrBackground">
-          <div className = "radio">
+        <form id="textOrBackground">
+          <div className="radio">
             <label>
               <input
-                type ="radio"
-                value = "text"
+                type="radio"
+                value="text"
                 checked={this.state.selectedOption === "text"}
-                onChange={this.handleOptionChange}/>
-                Text Color
+                onChange={this.handleOptionChange}
+              />
+              Text Color
             </label>
           </div>
 
-          <div className = "radio">
+          <div className="radio">
             <label>
               <input
-                type ="radio"
-                value = "background"
+                type="radio"
+                value="background"
                 checked={this.state.selectedOption === "background"}
-                onChange={this.handleOptionChange}/>
-                Background Color
+                onChange={this.handleOptionChange}
+              />
+              Background Color
             </label>
           </div>
         </form>
 
-
         <div className="colorPalette">
           {colors.map((color, ind) => {
             return (
-
-              <button onClick={()=>this.chooseColor({color})} className={color} key={ind} type="button" value={this.props.selectedColor} >
+              <button
+                onClick={() => this.chooseColor({ color })}
+                className={color}
+                key={ind}
+                type="button"
+                value={this.props.selectedColor}
+              >
                 {color}
               </button>
             );
           })}
         </div>
 
-          <h6>CSS CODE</h6>
-        <div id="codepreview" >
-        {codeSnippet} black;{` }`}
+        <h6>CSS CODE</h6>
+        <div id="codepreview">
+          <ul>
+            <li>{`{ color: ${this.state.color};`}</li>
+            <li>{`background: ${this.state.background}; }`}</li>
+          </ul>
         </div>
-
-
       </div>
     );
   }
@@ -103,8 +110,8 @@ const mapState = state => ({
 });
 
 const mapDispatch = dispatch => ({
-  pickColor: (color) => dispatch(pickColor(color))
-})
+  pickColor: color => dispatch(pickColor(color))
+});
 
 export default connect(
   mapState,
