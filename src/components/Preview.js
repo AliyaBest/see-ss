23ce0a8cb68208ab991f9e23eb693ac32789f54f
AliyaBest <aliyabest@gmail.com>
2020-11-30 14:28:09 -0400

    Deleted console.log

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,59 +1,84 @@
 import React from "react";
 
 export default class Preview extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      preview: ''
-    }
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.clearInput = this.clearInput.bind(this)
-
+      preview: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.clearInput = this.clearInput.bind(this);
   }
 
-  componentDidCatch(){
+  componentDidCatch() {
     this.setState({
       preview: this.state.preview
-    })
+    });
   }
 
-  handleChange(event){
-
+  handleChange(event) {
     this.setState({
       preview: event.target.value
-    })
+    });
   }
 
-  handleSubmit(event){
-    console.log('A name was submitted: ' + this.state.preview);
-
-    event.preventDefault()
+  handleSubmit(event) {
+    event.preventDefault();
   }
 
-  clearInput(){
-    document.getElementById('myInput').value = ''
+  clearInput() {
+    document.getElementById("myInput").value = "";
   }
 
-  render(){
-
-
+  render() {
     return (
       <div>
         <h6>PREVIEW</h6>
-        {this.state.preview.length ? <div id='preview-text' style={{height: 300, width: 400, borderStyle: 'solid', backgroundColor: 'white', fontSize: 48, padding: 20, borderColor: 'black'
-          }}>{this.state.preview} </div> :
-          <div id='preview-text' style={{height: 300, width: 400, borderStyle: 'solid', backgroundColor: 'white', padding: 20, fontSize: 48, borderColor: 'black'
-          }}>Lorem Ipsum</div>
+        {this.state.preview.length ? (
+          <div
+            id="preview-text"
+            style={{
+              height: 300,
+              width: 400,
+              borderStyle: "solid",
+              backgroundColor: "white",
+              fontSize: 48,
+              padding: 20,
+              borderColor: "black"
+            }}
+          >
+            {this.state.preview}{" "}
+          </div>
+        ) : (
+          <div
+            id="preview-text"
+            style={{
+              height: 300,
+              width: 400,
+              borderStyle: "solid",
+              backgroundColor: "white",
+              padding: 20,
+              fontSize: 48,
+              borderColor: "black"
+            }}
+          >
+            Lorem Ipsum
+          </div>
+        )}
 
-          }
-
-
-      <form onSubmit={this.handleSubmit}>
-        <input id="myInput" type="text" value={this.state.preview} onChange={this.handleChange}/>
-        <button onClick ={this.clearInput}type="submit">SUBMIT</button>
-      </form>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            id="myInput"
+            type="text"
+            value={this.state.preview}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.clearInput} type="submit">
+            SUBMIT
+          </button>
+        </form>
       </div>
-    )
+    );
   }
 }
